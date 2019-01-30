9cf14827b08361123565b4c960480e232250e5c3
Zee <zzeinabfaragg@gmail.com>
2019-01-30 02:14:14 -0500

    removed cors err

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,7 @@
 DROP DATABASE IF EXISTS moviedb;
 CREATE DATABASE moviedb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE IF NOT EXISTS `Calendars` (`id` INTEGER NOT NULL auto_increment , `username` VARCHAR(255), `movie` VARCHAR(255), `date` DATETIME, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`));
+
+
+CREATE TABLE IF NOT EXISTS `Favourites` (`id` INTEGER NOT NULL auto_increment , `username` VARCHAR(255), `movie` VARCHAR(255), `photo` TEXT, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`))
